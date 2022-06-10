0bb38d761903051c9eee98b8d0280e003d0c21b4
OwenOchieng <owen.ochieng@student.moringaschool.com>
2022-06-10 08:25:37 +0300

    initial commit

---
+++ index.js
@@ -0,0 +1,39 @@
+function add() {
+    // we'll fill this in shortly
+  }
+  function subtract() {
+    // we'll fill this in shortly
+  }
+  function multiply() {
+    // we'll fill this in shortly
+  } 
+  function divide() {
+    // we'll fill this in shortly
+  }  
+  function add(a, b) {
+    return a + b;
+  }  
+  function subtract(a, b) {
+    return a - b;
+  }  
+  function multiply(a, b) {
+    return a * b;
+  }  
+  function divide(a, b) {
+    return a / b;
+  }  
+  function increment(n) {
+    return n += 1
+  }
+  
+  function decrement(n) {
+    return n -= 1
+  }
+  
+  function makeInt(string) {
+    return parseInt(string, 10)
+  }
+  
+  function preserveDecimal(string) {
+    return parseFloat(string)
+  }
